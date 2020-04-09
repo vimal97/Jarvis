fed8d4e8e10e37e1687617b99f16226117376f71
vimal97 <vimal974ever2@gmail.com>
2020-04-10 00:20:18 +0530

    Added DatePicker for both Add Credit and Add Debit Activity

--- app/src/main/java/com/example/jarvis/AddCreditActivity.kt
+++ app/src/main/java/com/example/jarvis/AddCreditActivity.kt
@@ -1,15 +1,63 @@
 package com.example.jarvis
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.app.Dialog
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
 
 @Suppress("DEPRECATION")
 class AddCreditActivity : AppCompatActivity() {
+
+    private lateinit var dateView: Button
+    private lateinit var calendar: Calendar
+    private lateinit var datePicker: DatePicker
+    private var year: Int = 0
+    private var month: Int = 0
+    private var day: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_credit)
         supportActionBar?.title = "Add Credit"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //setting DatePicker Widget
+        dateView = findViewById<Button>(R.id.choose_date_credit);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    override fun onCreateDialog(id: Int): Dialog? { // TODO Auto-generated method stub
+        return if (id == 999) {
+            DatePickerDialog(
+                this,
+                myDateListener, year, month, day
+            )
+        } else null
+    }
+
+    private val myDateListener =
+        OnDateSetListener { arg0, arg1, arg2, arg3 ->
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate(arg1, arg2 + 1, arg3)
+        }
+
+    fun setDate(view: View?) {
+        showDialog(999)
+    }
+
+    private fun showDate(year: Int, month: Int, day: Int) {
+        dateView.text = StringBuilder().append("Chosen Date : ").append(day).append("/").append(month).append("/").append(year)
     }
 }
--- app/src/main/java/com/example/jarvis/AddDebitActivity.kt
+++ app/src/main/java/com/example/jarvis/AddDebitActivity.kt
@@ -1,14 +1,60 @@
 package com.example.jarvis
 
+import android.app.DatePickerDialog
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.DatePicker
+import java.util.*
 
 class AddDebitActivity : AppCompatActivity() {
+
+    private lateinit var dateView: Button
+    private lateinit var calendar: Calendar
+    private lateinit var datePicker: DatePicker
+    private var year: Int = 0
+    private var month: Int = 0
+    private var day: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_debit)
         supportActionBar?.title = "Add Debit"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //setting DatePicker Widget
+        dateView = findViewById<Button>(R.id.choose_date_debit);
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    override fun onCreateDialog(id: Int): Dialog? { // TODO Auto-generated method stub
+        return if (id == 999) {
+            DatePickerDialog(
+                this,
+                myDateListener, year, month, day
+            )
+        } else null
+    }
+
+    private val myDateListener =
+        DatePickerDialog.OnDateSetListener { arg0, arg1, arg2, arg3 ->
+            // TODO Auto-generated method stub
+            // arg1 = year
+            // arg2 = month
+            // arg3 = day
+            showDate(arg1, arg2 + 1, arg3)
+        }
+
+    fun setDate(view: View?) {
+        showDialog(999)
+    }
+
+    private fun showDate(year: Int, month: Int, day: Int) {
+        dateView.text = StringBuilder().append("Chosen Date : ").append(day).append("/").append(month).append("/").append(year)
     }
 }
