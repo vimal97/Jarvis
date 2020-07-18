5867cd33da1bc288d05231dfbc605120a2e5e5d9
vimal97 <vimal974ever2@gmail.com>
2020-07-19 00:58:41 +0530

    Secret notes integration completed with copy button

--- app/src/main/java/com/example/jarvis/SecretNotes.kt
+++ app/src/main/java/com/example/jarvis/SecretNotes.kt
@@ -2,9 +2,15 @@ package com.example.jarvis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_secret_notes.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SecretNotes : AppCompatActivity() {
 
@@ -15,6 +21,7 @@ class SecretNotes : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         var recyclerView = findViewById<RecyclerView>(R.id.secretNotesList)
         var sharedPreference = SharedPreference(this)
+//        sharedPreference.pushSecretNotesList("SecretNotes","")
         var gson = Gson()
         var secretNotesListString = sharedPreference.getSecretNotesList("SecretNotes")
         if(secretNotesListString != null){
@@ -22,12 +29,50 @@ class SecretNotes : AppCompatActivity() {
             var secretNotesList: MutableList<SecretNoteData> = mutableListOf()
             if (secretNotesListJson != null) {
                 for(i in secretNotesListJson){
-                    secretNotesList.add(gson.fromJson(i, SecretNoteData::class.java))
+                    if(i != ""){
+                        secretNotesList.add(gson.fromJson(i, SecretNoteData::class.java))
+                    }
                 }
             }
             recyclerView.adapter = SecretNotesAdapter(secretNotesList)
             recyclerView.layoutManager = LinearLayoutManager(applicationContext)
             recyclerView.setHasFixedSize(true)
         }
+    }
+
+    fun addSecretNote(view: View) {
+        var recyclerView = findViewById<RecyclerView>(R.id.secretNotesList)
+        val calendar = Calendar.getInstance();
+        val year = calendar.get(Calendar.YEAR);
+        val month = calendar.get(Calendar.MONTH) + 1;
+        val day = calendar.get(Calendar.DAY_OF_MONTH);
+        val today = "$day/$month/$year"
+        val sdf = SimpleDateFormat("hh:mm a")
+        val currentTime = sdf.format(Date())
+        val secretNote = SecretNoteData(findViewById<EditText>(R.id.secretNote).text.toString(), today, currentTime)
+        val sharedPreference = SharedPreference(this)
+        val gson = Gson()
+        val secretDataJson = gson.toJson(secretNote)
+        val secretNotesString = sharedPreference.getSecretNotesList("SecretNotes")
+        var secretNotesListString = listOf<String>()
+        if(secretNotesString == "null"){
+            sharedPreference.pushSecretNotesList("SecretNotes", secretDataJson)
+        }
+        else{
+            if(secretNotesString != null){
+                secretNotesListString = secretNotesString.split("|")
+                secretNotesListString += secretDataJson
+                sharedPreference.pushSecretNotesList("SecretNotes", secretNotesListString.joinToString("|"))
+            }
+        }
+        val secretNotesList = mutableListOf<SecretNoteData>()
+        for (i in secretNotesListString){
+            secretNotesList += gson.fromJson(i, SecretNoteData::class.java)
+        }
+        recyclerView.adapter = SecretNotesAdapter(secretNotesList)
+        recyclerView.layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.setHasFixedSize(true)
+        findViewById<EditText>(R.id.secretNote).setText("")
+        Log.v("Test_Vimal", secretDataJson)
     }
 }
--- app/src/main/java/com/example/jarvis/SecretNotesAdapter.kt
+++ app/src/main/java/com/example/jarvis/SecretNotesAdapter.kt
@@ -1,11 +1,17 @@
 package com.example.jarvis
 
+import android.app.PendingIntent.getActivity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.reminder_container.view.*
+
 
 class SecretNotesAdapter(private val secretNotes: List<SecretNoteData>): RecyclerView.Adapter<SecretNotesAdapter.SecretNotesHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SecretNotesHolder {
@@ -16,9 +22,21 @@ class SecretNotesAdapter(private val secretNotes: List<SecretNoteData>): Recycle
     override fun getItemCount(): Int = secretNotes!!.size
 
     override fun onBindViewHolder(holder: SecretNotesHolder, position: Int) {
-        holder.task.text = secretNotes[position].note
+        if(secretNotes[position].note.length >= 20){
+            holder.task.text = secretNotes[position].note.substring(0,20) + "..."
+        }
+        else{
+            holder.task.text = secretNotes[position].note
+        }
         holder.time.text = secretNotes[position].time
-        holder.time.text = secretNotes[position].date
+        holder.date.text = secretNotes[position].date
+        holder.itemView.findViewById<ImageButton>(R.id.copySecret).setOnClickListener {
+            val clipboard: ClipboardManager =
+                holder.itemView.context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clipData = ClipData.newPlainText("text", secretNotes[position].note)
+            Toast.makeText(holder.itemView.context, "Text Copied !!",Toast.LENGTH_SHORT).show()
+            clipboard.setPrimaryClip(clipData)
+        }
     }
 
     class SecretNotesHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
